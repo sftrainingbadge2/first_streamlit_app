2eec2ae5d1802e66364ea737180408c292355f94
sftrainingbadge2 <120451462+sftrainingbadge2@users.noreply.github.com>
2022-12-13 14:52:39 +0530

    added new menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,15 @@ streamlit.text('Omega3 & Blueberry Oat Meal')
 streamlit.text('Idli & Sambhar')
 
 streamlit.text('Pongal')
+
+streamlit.title('My MoMs new healthy diner')
+
+streamlit.header('Breakfast Favourites')
+
+streamlit.text('Poori and Potato curry')
+
+streamlit.text('Idli & Sambhar')
+
+streamlit.text('Chappathi and chenna')
+
+streamlit.title('Build your own smoothie')

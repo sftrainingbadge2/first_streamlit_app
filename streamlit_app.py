955e3eb5a01a2165a44a258198b79936f1be9fb7
sftrainingbadge2 <120451462+sftrainingbadge2@users.noreply.github.com>
2022-12-13 14:36:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega3 & Blueberry Oat Meal')
+
+streamlit.text('Idli & Sambhar')
+
+streamlit.text('Pongal')

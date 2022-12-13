69fce1aa06b17442509c68dd71527e6180d8c932
sftrainingbadge2 <120451462+sftrainingbadge2@users.noreply.github.com>
2022-12-13 15:01:25 +0530

    add csv content

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,6 @@ streamlit.text('Idli & Sambhar')
 streamlit.text('Chappathi and chenna')
 
 streamlit.title('Build your own smoothie')
+import pandas
+myfruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(myfruitlist)
